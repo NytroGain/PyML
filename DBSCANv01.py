2dbb2fa6737181ccf4de817b82d6600fa9571120
NytroGain <41354296+NytroGain@users.noreply.github.com>
2018-11-22 16:50:26 +0700

    Update DBSCANv01.py
    Add Evaluate?

--- DBSCANv01.py
+++ DBSCANv01.py
@@ -8,21 +8,42 @@ from sklearn.cluster import Birch
 from sklearn.cluster import MeanShift
 from sklearn.cluster import SpectralClustering
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn import datasets
+from sklearn import metrics
 
 
+#-----------------------------Load File-------------------------------------
 df = pd.read_csv('Book2.csv', sep=',', header=0)
 
 
+#-----------------------------PCA ------------------------------------------
 pca = PCA(n_components=2).fit(df)
 pca_2d = pca.transform(df)
 
-#DBSCAN
+
+#-----------------------------DBSCAN-----------------------------------------
 
 db = DBSCAN(eps=0.08, min_samples=3,algorithm='kd_tree', n_jobs=-1).fit(pca_2d)
 
-plt.scatter(pca_2d[:,0],pca_2d[:,1], c=db.labels_, cmap='rainbow') 
-plt.show()
+#----------------------------Plot Graph--------------------------------------
+#plt.scatter(pca_2d[:,0],pca_2d[:,1], c=db.labels_, cmap='rainbow') 
+#plt.show()
+
+
+#df['Cluster'] = db.labels_
+#df.to_csv('TestBook2.csv', index=False)
 #np.savetxt('Book2DBS.txt', db.labels_)
+
+#------------------------------Evaluate---------------------------------------
+#print("Homogeneity: %0.3f" % metrics.homogeneity_score(df, db.labels_))
+#print("Completeness: %0.3f" % metrics.completeness_score(df, db.labels_))
+#print("V-measure: %0.3f" % metrics.v_measure_score(df, db.labels_))
+#print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(df, db.labels_))
+#print("Adjusted Mutual Information: %0.3f" % metrics.adjusted_mutual_info_score(df, db.labels_))
+print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(df, db.labels_))
+
 print('----------------------------------------END DBSCAN----------------------------------------')
 #-----Birch
 #brc = Birch(branching_factor=2000, n_clusters=None, threshold=0.5,
