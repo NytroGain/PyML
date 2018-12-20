6242d9ff2c1b20bded6d73173bbef3d7b5b6ef03
NytroGain <41354296+NytroGain@users.noreply.github.com>
2018-12-20 16:05:35 +0700

    upd 20/12

--- Loadpickle.py
+++ Loadpickle.py
@@ -11,26 +11,38 @@ from sklearn.tree import export_graphviz
 import pydotplus
 import time
 import pickle
+import keras
+from keras.models import model_from_json
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+import h5py
 '''
 tn = pickle.load(open('dtree_model_pickle.p','rb'))
 print(tn)
 '''
-
+'''
 from sklearn.preprocessing import OneHotEncoder
 
 dataset = pd.read_csv('afterFeatureSelectionCSV.csv',sep=',',header=0, encoding='TIS-620')
 
 dataset = dataset.drop(['ACI'], axis=1)
 '''
+'''
 nn = pd.get_dummies(dataset)
 nns = nn.to_csv('OtestUTF.csv', index=False, encoding='UTF-8')
+'''
 '''
 enc = OneHotEncoder(sparse=False)
 nd = enc.fit_transform(dataset)
 print(nd)
 '''
+''''
 nd = pd.DataFrame(test)
 na = nd.to_csv('OnTest.csv', index=False)
-#nn = dataset.to_csv('OneHottest.csv', index=False)
-
 '''
+#nn = dataset.to_csv('OneHottest.csv', index=False)
+from keras.models import Sequential
+model = Sequential()
+loaded_model=model.load_weights('Neural_2.h5')
+print(loaded_model)
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,6 +8,7 @@ import time
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split  
 from sklearn.metrics import accuracy_score , confusion_matrix , precision_score, recall_score, f1_score, classification_report
+import pickle
 #-----------------------------------------------------Read CSV File
 start_time = time.time()
 dataset = pd.read_csv('afterFeatureSelectionCSV.csv',sep=',',header=0, encoding='TIS-620')
@@ -45,17 +46,25 @@ model.add(Dense(1,init='uniform',activation='sigmoid'))
 model.compile(optimizer='adam',loss="binary_crossentropy",metrics=['accuracy'])
 
 #-----------------------------------K-Fold Cross Validation
-kf = KFold(n_splits=10)
-KFold(n_splits=10, random_state=None, shuffle=False)
+'''
+kf = KFold(n_splits=5)
+KFold(n_splits=5, random_state=None, shuffle=False)
 
 score_array =[]
 for train_index, test_index in kf.split(X):
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-    model.fit(X_train,y_train,batch_size=10, nb_epoch=10)
-    y_pred = model.predict_classes(X_test)
-    score_array.append(accuracy_score(y_test, y_pred))
+   '''
 
+score_array =[]
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)  
+model.fit(X_train,y_train,batch_size=10, nb_epoch=300)
+y_pred = model.predict_classes(X_test)
+score_array.append(accuracy_score(y_test, y_pred))
+k = pd.DataFrame(X_train)
+l = pd.DataFrame(y_pred)
+jk = k.to_csv('X_train.csv', index=False)
+jl = l.to_csv('y_pred.csv', index=False)
 avg_score = np.mean(score_array,axis=0)
 each_score = pd.DataFrame(score_array,columns=['Each Round'])
 each_score.index = each_score.index+1
@@ -73,8 +82,13 @@ print("Accuracy Mean = ",avg_score)
 print("---Runtime %s seconds ---" % (time.time() - start_time))
 
 
+#open('Neural_T20.h5', 'wb')
 
 
-
+# Save the model in h5 format 
+model.save('TestModelT300.h5')
+'''
+model.save_weights('Neural_T20.h5')
+'''
 print("----------------------------------------------End------------------------------------------")
 #------------------------------------------------------
